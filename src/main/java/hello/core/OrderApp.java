f364401ce197da557431c48ed5c7ff81d2e5a4fa
이은우 <80537533+lew0205@users.noreply.github.com>
2022-06-28 20:27:51 +0900

    DIP

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -10,15 +10,19 @@ import hello.core.order.OrderServiceImpl;
 
 public class OrderApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
-        OrderService orderService = new OrderServiceImpl();
+
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        OrderService orderService = appConfig.orderService();
+//        MemberService memberService = new MemberServiceImpl(null);
+//        OrderService orderService = new OrderServiceImpl(null,null);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.joinMember(member);
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
- 
+
         System.out.println("order = " + order);
         System.out.println("order.calculatePrice = " + order.calculatePrice());
     }
